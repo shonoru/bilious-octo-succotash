5ed52877cb093e838657b9ad941908d330c5271a
Andrey Bodoev <shonoru@gmail.com>
2015-10-25 23:48:14 +0800

    Add scripts
    * Initial files

---
+++ scripts/products-en.js
@@ -0,0 +1,28 @@
+// TODO real words
+var fs = require('fs');
+
+var Chance = require('chance'),
+    chance = new Chance();
+
+function generateRecord() {
+  var str = ';';
+  str += chance.sentence({words: 3}) + ';' + chance.sentence() + ';' + chance.paragraph();
+  return str;
+}
+
+var count = 10000;
+var timestamp = chance.timestamp();
+
+var stream = fs.createWriteStream('products-' + timestamp +'.txt');
+stream.once('open', function(fd) {
+  for (var i = 0; i < 100; i++) {
+    var record = ';';
+    record += count + i;
+    record += generateRecord();
+    record += '\n';
+
+    stream.write(record);
+  }
+
+  stream.end();
+});
---
+++ scripts/products.js
@@ -0,0 +1,2 @@
+// ;23191;1288, brand_5;Sony;ACC-CSPW;pieces;
+// ;COUNT;categorie, supplier; supplier.name;random;pieces;
